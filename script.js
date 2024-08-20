2fe286ef3ad0b82d2c4af9b829c3ec5c0860257f
JeromeMaligaya <jeromehapi_97@yahoo.com>
2024-08-20 23:01:46 +0200

    added currentActive

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const galleryIndex = document.getElementById('gallery');
 
 //2.raccolta dati
 const sources = ['img/01.webp', 'img/02.webp', 'img/03.webp', 'img/04webp', 'img/05.webp'];
+
     
 // 3.lavorazione dati
 // tramite template literal inizio a costruire la stringa per le immagini
@@ -23,8 +24,17 @@ for (let i=0; i < sources.length; i++){
     image = sources[i];
     images += `<img src="${image}"> alt="picture-${i + 1}" `;
 }
-
 console.log(images);
+
+// richiamo tutte le img presenti dentro #gallery
+const imgIndex = document.querySelectorAll('#gallery img');
+
+// creo una variabile da concatenare, che farà apparire la img desiderata tramite una classe presente nel css (active)
+let currentActive = 0;
+imgIndex[currentActive].classList.add('active');
+
+
+
     
 
 // 4.generazione output
